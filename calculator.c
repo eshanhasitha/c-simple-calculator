a2b4c248745d6b47a9ee429863f359c7b9e05644
Eshanhasitha <eshanhasitha5@gmail.com>
2025-03-19 03:16:04 +0530

    Modified the code for multiple inputs

--- calculator.c
+++ calculator.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 int main() {
-int num1,num2;
-//get inputs from user
-printf("Enter Number 1: ");
-scanf("%d",&num1);
-printf("Enter Number 2: ");
-scanf("%d",&num2);
-printf("___Chose oparation___ \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Modulus \n");
-int op;
-scanf("%d",&op);
+int num,num1,sum;
 
-//opatations
-switch (op)
-{
-case 1:
-    printf("Result: %d",num1+num2);
-    break;
-case 2:
-    printf("Result: %d",num1-num2);
-    break;  
-case 3:
-    printf("Result: %d",num1*num2);
-    break;
-case 4:
-    printf("Result: %d",num1/num2);
-    break;
-case 5:
-    printf("Result: %d",num1%num2);
-    break;
-default:
-    printf("Invalid Oparation");
-    break;
+//get inputs from user
+printf("___Simple Calculator___\n");
+printf("Enter the number of inputs: ");
+scanf("%d",&num);
+
+for(int i=1;i<=num;i++){
+    printf("Enter Number %d: ",i);
+    scanf("%d",&num1);
+   
+    printf("___Chose oparation___ \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Modulus \n");
+    int op;
+    scanf("%d",&op);
+
+    //opatations
+    switch (op)
+    {
+    case 1:
+        printf("Result: %d\n",sum=sum+num1);
+        break;
+    case 2:
+        printf("Result: %d\n",sum=sum-num1);
+        break;  
+    case 3:
+        sum=1;
+        printf("Result: %d\n",sum=sum*num1);
+        break;
+    case 4:
+        sum=1;
+        printf("Result: %d\n",sum=sum/num1);
+        break;
+    case 5:
+        sum=1;
+        printf("Result: %d\n",sum=sum%num1);
+        break;
+    default:
+        printf("Invalid Oparation");
+        break;
+    }
 }
 
 return 0;
